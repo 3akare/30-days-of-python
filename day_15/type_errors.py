8fe81014b44667770ae2725dfb29b33168ae3a51
bakare <bakaredavid007@gmail.com>
2022-11-02 14:37:24 +0100

    day 15

---
+++ day_15/type_errors.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+'''
+    day 15
+    30 days of python
+'''
+
+SyntaxError
+NameError
+IndexError
+ModuleNotFoundError
+AttributeError
+KeyError
+TypeError
+ImportError
+ValueError
+ZeroDivisionError
